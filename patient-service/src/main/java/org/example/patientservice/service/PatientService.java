e9dc41aa0e498fd055afc1d50cb93a890feb0ead
Shivam Patel <shivamyana84a@gmail.com>
2025-04-18 20:28:32 +0530

    Added PatientService and PatientRepository

---
+++ patient-service/src/main/java/org/example/patientservice/service/PatientService.java
@@ -0,0 +1,20 @@
+package org.example.patientservice.service;
+
+import org.example.patientservice.model.Patient;
+import org.example.patientservice.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PatientService {
+    private PatientRepository patientRepository;
+
+    public PatientService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    public List<PatientResponseDTO> getPatients(){
+        List<Patient> patients = patientRepository.findAll();
+    }
+}
