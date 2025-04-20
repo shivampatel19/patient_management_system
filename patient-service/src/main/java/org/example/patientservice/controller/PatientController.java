2d4850ee02b7dd40917b593ee5ad600a7650778d
Shivam Patel <shivamyana84a@gmail.com>
2025-04-21 01:28:06 +0530

    Added create patient logic

---
+++ patient-service/src/main/java/org/example/patientservice/controller/PatientController.java
@@ -0,0 +1,33 @@
+package org.example.patientservice.controller;
+
+import jakarta.validation.Valid;
+import org.example.patientservice.dto.PatientRequestDTO;
+import org.example.patientservice.dto.PatientResponseDTO;
+import org.example.patientservice.service.PatientService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PatientResponseDTO>> getPatients() {
+        List<PatientResponseDTO> patients = patientService.getPatients();
+        return ResponseEntity.ok().body(patients);
+    }
+
+    @PostMapping
+    public ResponseEntity<PatientResponseDTO> createPatient(@Valid @RequestBody PatientRequestDTO patientRequestDTO) {
+        PatientResponseDTO patientResponseDTO = patientService.createPatient(patientRequestDTO);
+
+        return ResponseEntity.ok().body(patientResponseDTO);
+    }
+}
--- patient-service/src/main/java/org/example/patientservice/service/PatientService.java
+++ patient-service/src/main/java/org/example/patientservice/service/PatientService.java
@@ -1,5 +1,8 @@
 package org.example.patientservice.service;
 
+import org.example.patientservice.dto.PatientRequestDTO;
+import org.example.patientservice.dto.PatientResponseDTO;
+import org.example.patientservice.mapper.PatientMapper;
 import org.example.patientservice.model.Patient;
 import org.example.patientservice.repository.PatientRepository;
 import org.springframework.stereotype.Service;
@@ -16,5 +19,14 @@ public class PatientService {
 
     public List<PatientResponseDTO> getPatients(){
         List<Patient> patients = patientRepository.findAll();
+
+        return patients.stream()
+                .map(PatientMapper::toDTO).toList();
+    }
+
+    public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO) {
+        Patient newPatient = patientRepository.save(
+                PatientMapper.toModel(patientRequestDTO));
+        return PatientMapper.toDTO(newPatient);
     }
 }
