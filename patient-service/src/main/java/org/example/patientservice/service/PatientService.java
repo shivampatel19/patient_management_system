01f93b25410d0aea748fe33a0b369afc2b811759
Shivam Patel <shivamyana84a@gmail.com>
2025-04-21 11:40:47 +0530

    Custom Exception Handling

--- patient-service/src/main/java/org/example/patientservice/service/PatientService.java
+++ patient-service/src/main/java/org/example/patientservice/service/PatientService.java
@@ -2,6 +2,7 @@ package org.example.patientservice.service;
 
 import org.example.patientservice.dto.PatientRequestDTO;
 import org.example.patientservice.dto.PatientResponseDTO;
+import org.example.patientservice.exception.EmailAlreadyExistsException;
 import org.example.patientservice.mapper.PatientMapper;
 import org.example.patientservice.model.Patient;
 import org.example.patientservice.repository.PatientRepository;
@@ -11,7 +12,7 @@ import java.util.List;
 
 @Service
 public class PatientService {
-    private PatientRepository patientRepository;
+    private final PatientRepository patientRepository;
 
     public PatientService(PatientRepository patientRepository) {
         this.patientRepository = patientRepository;
@@ -25,6 +26,11 @@ public class PatientService {
     }
 
     public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO) {
+        if(patientRepository.existsByEmail(patientRequestDTO.getEmail())){
+            throw new EmailAlreadyExistsException("A patient with this email " +
+                    "already exists : " + patientRequestDTO.getEmail());
+        }
+
         Patient newPatient = patientRepository.save(
                 PatientMapper.toModel(patientRequestDTO));
         return PatientMapper.toDTO(newPatient);
