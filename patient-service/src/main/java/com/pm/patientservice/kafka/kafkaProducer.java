48741efa305cbea03f51afe28562a81a2237dfa4
ShivamPatel <shivamyana84a@gmail.com>
2025-04-26 22:08:23 +0530

    kafka init

---
+++ patient-service/src/main/java/com/pm/patientservice/kafka/kafkaProducer.java
@@ -0,0 +1,16 @@
+package com.pm.patientservice.kafka;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class kafkaProducer {
+
+    private final KafkaTemplate<String, byte[]> kafkaTemplate;
+
+    public kafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+
+}
